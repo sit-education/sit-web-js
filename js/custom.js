3a796b46aacd0ba3b0cc03f615f602d3a1b257ff
Anton Burkun <burkun.anton@gmail.com>
2016-05-03 17:35:00 +0300

    fix bugs

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 'use strict';
 $(document).ready(function () {
 
-    var URI ='https://sit-todo-test.appspot.com/api/v1/';
+    var URI = 'https://sit-todo-test.appspot.com/api/v1/';
     var TOKEN_NAME = 'access-token';
     var SOME_ERROR = 'Something was wrong. Please try later';
     var jsonTaskData = 0;
@@ -10,11 +10,11 @@ $(document).ready(function () {
     $('form button').prop('disabled', false);
 
     var arrPagePath = [];
-    var currentPage= window.location.pathname;
+    var currentPage = window.location.pathname;
     arrPagePath = currentPage.split('/');
-    currentPage = arrPagePath[arrPagePath.length-1];
+    currentPage = arrPagePath[arrPagePath.length - 1];
 
-    function getValueFromCookie(nameOfCookie){
+    function getValueFromCookie(nameOfCookie) {
         var arrCookies = [];
         var dictionaryCookies = {};
         var oneCookie = [];
@@ -22,39 +22,33 @@ $(document).ready(function () {
         var cookie = document.cookie;
         arrCookies = document.cookie.split('; ');
 
-        if(!(document.cookie)){
+        if (!(document.cookie)) {
             return false;
         }
 
-        for (var i = 0; i < arrCookies.length; i++){
+        for (var i = 0; i < arrCookies.length; i++) {
             oneCookie = arrCookies[i].split('=');
             dictionaryCookies[oneCookie[0]] = oneCookie[1];
 
         }
-        if(dictionaryCookies[nameOfCookie]){
+        if (dictionaryCookies[nameOfCookie]) {
             return dictionaryCookies[nameOfCookie];
-        }else{
+        } else {
             return false;
         }
 
     }
 
 
-
     var accessToken = getValueFromCookie(TOKEN_NAME);
 
-    if (currentPage == 'tickets.html'){
-        if(!(accessToken)){
+    if (currentPage == 'tickets.html') {
+        if (!(accessToken)) {
             window.location.href = 'index.html';
         }
+        //setInterval(getTask, 2000);
         getTask();
     }
-
-
-
-
-
-
 
 
     //function checkSamePass(passwordField, confPassField) {
@@ -66,8 +60,8 @@ $(document).ready(function () {
     //    return mailPattern.test(email);
     //}
 
-    function identifyErrorAnswer(error){
-        if(!(error.status)){
+    function identifyErrorAnswer(error) {
+        if (!(error.status)) {
             var response = 0;
             var totalErrorResponse = 0;
             response = JSON.parse(error.responseText);
@@ -76,36 +70,36 @@ $(document).ready(function () {
                 errorMessage: response.errors[0].error_message
             };
             $('form button').before('<span class="invalid-message">' + totalErrorResponse.errorMessage + '</span>');
-        }else{
+        } else {
             $('form button').before('<span class="invalid-message">' + SOME_ERROR + '</span>');
         }
     }
 
-    function authOfUser(userFormData){
+    function authOfUser(userFormData) {
         $.ajax({
             type: 'POST',
             url: URI + 'login',
             data: userFormData,
             contentType: 'application/json',
-            success: function(data){
-                if(data.status){
+            success: function (data) {
+                if (data.status) {
                     var accessToken = data.data.tokenKey;
                     var tokenExpired = (data.data.tokenExpired) * 1000;
                     tokenExpired = new Date(tokenExpired).toUTCString();
                     //document.cookie = TOKEN_NAME + '=' + accessToken + '; path=/; expires=' + tokenExpired;
                     document.cookie = TOKEN_NAME + '=' + accessToken + '; path=/';
                     window.location.href = 'tickets.html';
-                }else{
-                    $('form button').before('<span class="invalid-message">'+ SOME_ERROR +'</span>');
+                } else {
+                    $('form button').before('<span class="invalid-message">' + SOME_ERROR + '</span>');
                 }
             },
-            error: function(error){
+            error: function (error) {
                 identifyErrorAnswer(error);
             }
         });
-        setTimeout(function(){
+        setTimeout(function () {
             $('form button').prop('disabled', false);
-        },2000);
+        }, 2000);
 
     }
 
@@ -116,13 +110,13 @@ $(document).ready(function () {
             data: registerFormData,
             contentType: 'application/json',
             success: function (data) {
-                if(data.status){
-                    $('form').fadeOut(500, function(){
+                if (data.status) {
+                    $('form').fadeOut(500, function () {
                         $('.after-action').fadeIn(500);
                     });
 
-                }else{
-                    $('form button').before('<span class="invalid-message">'+ SOME_ERROR +'</span>');
+                } else {
+                    $('form button').before('<span class="invalid-message">' + SOME_ERROR + '</span>');
                 }
             },
             error: function (error) {
@@ -132,30 +126,30 @@ $(document).ready(function () {
         });
     }
 
-    function recoverPass(recoverFormData){
+    function recoverPass(recoverFormData) {
         $.ajax({
             type: 'POST',
             url: URI + 'restorePassword',
             data: recoverFormData,
             headers: {
-                'Content-Type':'application/json'
+                'Content-Type': 'application/json'
             },
             success: function (data) {
-                if(data.status){
-                    $('form').fadeOut(500, function(){
+                if (data.status) {
+                    $('form').fadeOut(500, function () {
                         $('.after-action').fadeIn(500);
                     });
-                }else{
-                    $('form button').before('<span class="invalid-message">'+ SOME_ERROR +'</span>');
+                } else {
+                    $('form button').before('<span class="invalid-message">' + SOME_ERROR + '</span>');
                 }
             },
-            error: function(error){
+            error: function (error) {
                 identifyErrorAnswer(error);
             }
         });
     }
 
-    function addTask(addTaskFormData){
+    function addTask(addTaskFormData) {
         $.ajax({
             type: 'POST',
             url: URI + 'item',
@@ -164,22 +158,22 @@ $(document).ready(function () {
                 'Token-Key': accessToken
             },
             data: addTaskFormData,
-            success: function(data){
-                if(data.status){
+            success: function (data) {
+                if (data.status) {
                     window.location.href = 'tickets.html';
-                }else{
-                    $('form button').before('<span class="invalid-message">'+ SOME_ERROR +'</span>');
+                } else {
+                    $('form button').before('<span class="invalid-message">' + SOME_ERROR + '</span>');
                     console.log(data);
                 }
             },
-            error: function(error){
+            error: function (error) {
                 identifyErrorAnswer(error);
             }
         });
     }
 
-    function getTask(){
-         $.ajax({
+    function getTask() {
+        $.ajax({
             type: 'GET',
             url: URI + 'items',
             headers: {
@@ -187,111 +181,110 @@ $(document).ready(function () {
                 'Token-Key': accessToken
             },
             success: function (data) {
-                if(data.status){
-                    var arrOfTasks = data.data.todoData;
+                if (data.status) {
                     $('.task').remove();
-                    if(arrOfTasks.length){
-                        arrOfTasks.forEach(function(item, i){
+                    var arrOfTasks = data.data.todoData;
+                    if (arrOfTasks.length) {
+                        arrOfTasks.forEach(function (item, i) {
                             var dictionaryTask = {};
                             dictionaryTask = arrOfTasks[i];
                             $('#tasks-list-section .task-list').append(
                                 '<div class="task">' +
-                                    '<div class="column number">'+ (++i) +'</div>'+
-                                    '<div class="column content">' +
-                                        '<span class="title">'+ dictionaryTask.title +'</span>'+
-                                        '<span class="description">'+ dictionaryTask.description +'</span>'+
-                                    '</div>'+
-                                    '<div class="column control">' +
-                                        '<span class="edit glyphicon glyphicon-pencil"></span>'+
-                                        '<span class="delete glyphicon glyphicon-trash"></span>'+
-                                '   </div>'+
+                                    '<div class="column number">' + (++i) + '</div>' +
+                                '<div class="column content">' +
+                                    '<span class="title">' + dictionaryTask.title + '</span>' +
+                                    '<span class="description">' + dictionaryTask.description + '</span>' +
+                                '</div>' +
+                                '<div class="column control">' +
+                                    '<span class="edit glyphicon glyphicon-pencil"></span>' +
+                                    '<span class="delete glyphicon glyphicon-trash"></span>' +
+                                '   </div>' +
                                 '</div>'
                             );
-                            $('.task').fadeTo(700, 1);
                         });
 
-                    }else{
+                    } else {
                         $('#tasks-list-section .task-list').append(
-                            '<div class="task note">'+
-                                '<h2>You haven\'t any tasks yet</h2>'+
+                            '<div class="task note">' +
+                                '<h2>You haven\'t any tasks yet</h2>' +
                             '</div>'
                         );
                     }
 
                     var allTasksDom = $('.task-list');
                     successTaskList(arrOfTasks, allTasksDom);
-                }else{
-                    $('form button').before('<span class="invalid-message">'+ SOME_ERROR +'</span>');
+                } else {
+                    $('form button').before('<span class="invalid-message">' + SOME_ERROR + '</span>');
                     console.log(data);
                 }
             },
-            error: function(error){
+            error: function (error) {
                 identifyErrorAnswer(error);
             }
 
         });
+
     }
 
-    function successTaskList(arrOfTasks, allTasksDom){
+    function successTaskList(arrOfTasks, allTasksDom) {
         var activeItemValue = 0;
         var jsonTaskData = '';
 
-        $('.new-task').on('click', function(){
+        $('.new-task').on('click', function () {
             $('.new-task').prop('disabled', true);
             $('#edit-task-section').remove();
-            $('#tasks-list-section').fadeOut(0, function(){
+            $('#tasks-list-section').fadeOut(0, function () {
                 $('#add-task-section').fadeIn();
             });
         });
 
-        $('.task-list').on('click', '.delete', function (){
+        $('.task-list').on('click', '.delete', function () {
             $('.delete').prop('disabled', true);
             var activeTaskDom = $(this).parents('.task');
             activeItemValue = $(activeTaskDom).find('.number').text();
-            taskId = arrOfTasks[activeItemValue-1].id;
+            taskId = arrOfTasks[activeItemValue - 1].id;
 
             deleteTask(taskId, activeTaskDom);
-            arrOfTasks.splice(activeItemValue-1,1);
+            arrOfTasks.splice(activeItemValue - 1, 1);
             updateTaskNumber(allTasksDom);
         });
 
-        $('.task-list').on('click', '.edit', function(){
+        $('.task-list').on('click', '.edit', function () {
             $('.edit').prop('disabled', true);
-            $('#tasks-list-section').fadeOut(0, function(){
+            $('#tasks-list-section').fadeOut(0, function () {
                 $('#edit-task-section').fadeIn();
             });
 
             var activeTaskDom = $(this).parents('.task');
             activeItemValue = $(activeTaskDom).find('.number').text();
             var currentObjFromArr = {};
-            currentObjFromArr = arrOfTasks[activeItemValue-1];
+            currentObjFromArr = arrOfTasks[activeItemValue - 1];
             taskId = currentObjFromArr.id;
-            jsonTaskData =  getTaskDataForEdit(currentObjFromArr);
+            jsonTaskData = getTaskDataForEdit(currentObjFromArr);
             $('#editTitle').val(currentObjFromArr.title);
             $('#editDesc').val(currentObjFromArr.description);
         });
 
     }
 
-    function getTaskDataForEdit(currentObjFromArr){
+    function getTaskDataForEdit(currentObjFromArr) {
         delete currentObjFromArr.id;
         jsonTaskData = JSON.stringify(currentObjFromArr);
         return jsonTaskData;
     }
 
-    function updateTaskNumber(allTasksDom){
-        setTimeout(function(){
+    function updateTaskNumber(allTasksDom) {
+        setTimeout(function () {
             $('.delete').prop('disabled', false);
-            $(allTasksDom).find('.number').each(function(index){
+            $(allTasksDom).find('.number').each(function (index) {
                 index++;
                 $(this).text(index);
             });
-        },1300)
+        }, 1300)
     }
 
 
-
-    function editTask(editFormData, taskId){
+    function editTask(editFormData, taskId) {
         $.ajax({
             type: 'PUT',
             url: URI + 'item/' + taskId,
@@ -300,21 +293,21 @@ $(document).ready(function () {
                 'Token-Key': accessToken
             },
             data: editFormData,
-            success: function(data){
-                if(data.status){
+            success: function (data) {
+                if (data.status) {
                     window.location.href = 'tickets.html';
-                }else{
-                    $('form button').before('<span class="invalid-message">'+ SOME_ERROR +'</span>');
+                } else {
+                    $('form button').before('<span class="invalid-message">' + SOME_ERROR + '</span>');
                     console.log(data);
                 }
             },
-            error: function(error){
+            error: function (error) {
                 identifyErrorAnswer(error);
             }
         });
     }
 
-    function deleteTask(taskId, activeTaskDom){
+    function deleteTask(taskId, activeTaskDom) {
         $.ajax({
             type: 'DELETE',
             url: URI + 'item/' + taskId,
@@ -322,18 +315,18 @@ $(document).ready(function () {
                 'Content-Type': 'application/json',
                 'Token-Key': accessToken
             },
-            success: function(data){
-                if(data.status){
-                    $(activeTaskDom).fadeOut(400, function(){
+            success: function (data) {
+                if (data.status) {
+                    $(activeTaskDom).fadeOut(400, function () {
                         $(this).remove();
                     });
 
-                }else{
-                    $('form button').before('<span class="invalid-message">'+ SOME_ERROR +'</span>');
+                } else {
+                    $('form button').before('<span class="invalid-message">' + SOME_ERROR + '</span>');
                     console.log(data);
                 }
             },
-            error: function(error){
+            error: function (error) {
                 identifyErrorAnswer(error);
             }
         });
@@ -356,7 +349,7 @@ $(document).ready(function () {
         formData = JSON.stringify(objFormData);
         alert(formData);
 
-        switch(idForm){
+        switch (idForm) {
             case 'login':
                 authOfUser(formData);
                 break;
@@ -378,9 +371,9 @@ $(document).ready(function () {
                 break;
         }
 
-        setTimeout(function(){
+        setTimeout(function () {
             $('form button').prop('disabled', false);
-        },2000);
+        }, 2000);
 
 
         //if ($(this).find('button').attr('name') == 'sign-up'){
@@ -394,12 +387,6 @@ $(document).ready(function () {
         //if ($(this).find('button').attr('name') == 'recover-pass-btn'){
         //    recoverPass(formData);
         //}
-
-
-
-
-
-
 
 
         //$(this).find('input[required]').each(function () {
@@ -454,24 +441,24 @@ $(document).ready(function () {
 
     });
 
-    $('#logout-btn').on('click', function(){
+    $('#logout-btn').on('click', function () {
         $.ajax({
             type: 'POST',
             url: URI + 'logout',
             headers: {
                 'Token-Key': accessToken,
-                'Content-Type':'application/json'
+                'Content-Type': 'application/json'
             },
-            success: function(data){
-                if(data.status){
+            success: function (data) {
+                if (data.status) {
                     document.cookie = TOKEN_NAME + '=; path=/; expires= Thu, 01 Jan 1970 00:00:01 GMT';
                     window.location.href = 'index.html';
-                }else{
+                } else {
                     alert('Sorry an error occurred on server');
                     console.log(data);
                 }
             },
-            error: function (error){
+            error: function (error) {
                 alert('Sorry an error occurred on server');
                 console.log(error);
             }
@@ -479,8 +466,6 @@ $(document).ready(function () {
 
         });
     });
-
-
 
 
 });
